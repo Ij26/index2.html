2b7319756287dd84361162a781cb5c6fecd361ae
Ij26 <114659283+Ij26@users.noreply.github.com>
2023-01-02 19:13:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,401 @@
+const questions = [
+  {
+    question:
+      "Which among the following Kavya of Sanskrit, deal with court intrigues & access to power of Chandragupta Maurya? ?",
+    optionA: "Mrichhakatika",
+    optionB: "Ritusamhara",
+    optionC: "Kumarasambhava",
+    optionD: "Mudrarakshahsa",
+    correctOption: "optionD",
+  },
+
+  {
+    question: "Which of the following statement is wrong? ?",
+    optionA: " Sunga dynasty was founded by pushyamitra",
+    optionB: " Ashoka the great Mauryan king died in 332 BC",
+    optionC: "Ashoka invaded the kalinga in 261 BC",
+    optionD: " Chandragupta Maurya earned the title of the Liberator.",
+    correctOption: "optionB",
+  },
+
+  {
+    question:
+      " On which of the following systems of Hindu Philosophy , Shankaracharya wrote commentary in 9th century AD? ?",
+    optionA: " Sankhya",
+    optionB: "Vaisheshika",
+    optionC: "Yoga",
+    optionD: " Uttarmimansa",
+    correctOption: "optionD",
+  },
+
+  {
+    question:
+      " Who was the first Indian ruler who had territory outside India?",
+    optionA: "Ashoka",
+    optionB: "Chandragupta Mauryar",
+    optionC: " Kanishka",
+    optionD: "Huvishka",
+    correctOption: "optionC",
+  },
+
+  {
+    question:
+      "Which king started the organization of Kumbh fair at Allahabad??",
+    optionA: "Akabar",
+    optionB: " Dhruvasena Ii",
+    optionC: "Narshimhvarman",
+    optionD: " Harshavardhana",
+    correctOption: "optionD",
+  },
+
+  {
+    question: " Where were the hymns of Rigveda composed? ?",
+    optionA: " Punjab",
+    optionB: "Gujarat",
+    optionC: "Rajasthan",
+    optionD: "Uttar Pradesh",
+    correctOption: "optionA",
+  },
+
+  {
+    question: " Which empire lasted the longest among the following ?",
+    optionA: "The Palas",
+    optionB: "The Pratiharas",
+    optionC: " The Rashtrakutas",
+    optionD: "The Senas",
+    correctOption: "optionC",
+  },
+
+  {
+    question: "Who was the last Hindu emperor of northern India?",
+    optionA: "Harsha",
+    optionB: " Pulakesin II",
+    optionC: " Rajyavardhana",
+    optionD: "Skandagupta",
+    correctOption: "optionA",
+  },
+
+  {
+    question:
+      "On which river the Rig Veda and Indus Civilizations were based ?",
+    optionA: "Narmada",
+    optionB: "Ganga",
+    optionC: "Godavari",
+    optionD: "Saraswati",
+    correctOption: "optionD",
+  },
+
+  {
+    question: `"What led to the end of Indus Valley Civilization?`,
+    optionA: "Invasion of Aryans",
+    optionB: "Recurrent Floods",
+    optionC: "Earthquakes",
+    optionD: "All the above",
+    correctOption: "optionD",
+  },
+
+  {
+    question: " Which among the following is not correct ?",
+    optionA: "The capital of pandyas was Madurai",
+    optionB: " The capital of Cheras was Vanchi",
+    optionC: " Capital of the Videha Kingdom – Mithila",
+    optionD: "Capital of Gahadwal Dynasty – Kannauj",
+    correctOption: "optionC",
+  },
+
+  {
+    question: "Who was the main male God worshipped by Indus people ?",
+    optionA: " Lord Vishnu",
+    optionB: "Vishnu",
+    optionC: "Brahma",
+    optionD: "Indra",
+    correctOption: "optionA",
+  },
+
+  {
+    question:
+      "Which among the following was the royal emblem of Chera Kingdom?",
+    optionA: "Tiger",
+    optionB: "Bow",
+    optionC: "Fish",
+    optionD: "Carp",
+    correctOption: "optionB",
+  },
+
+  {
+    question:
+      "handragupta Maurya under the able guidance of Kautilya uprooted which of the following Kings ?",
+    optionA: "Shishunaga",
+    optionB: "Kalasoka",
+    optionC: "Mahapadma Nanda",
+    optionD: "Dhanananda",
+    correctOption: "optionD",
+  },
+
+  {
+    question:
+      "Buddha established an order of monks and nuns which was known as ?",
+    optionA: " Sangha",
+    optionB: "Jana",
+    optionC: "Vidhatha",
+    optionD: " Kirat",
+    correctOption: "optionA",
+  },
+
+  {
+    question:
+      "Which Harappan site in below had direct sea trade links with Mesopotamia ?",
+    optionA: "Kot Diji",
+    optionB: "Dholavira",
+    optionC: "Lothal",
+    optionD: "Ropar",
+    correctOption: "optionC",
+  },
+
+  {
+    question:
+      "The Asirgadh Inscription in Nimar district of Madhya Pradesh describes the victory of which of the following kings?",
+    optionA: "Sarvavarmana",
+    optionB: " Advaita Varmana",
+    optionC: "Grahavarmana",
+    optionD: "Avanti Varmana",
+    correctOption: "optionA",
+  },
+
+  {
+    question: "The Rig Veda consists of how many hymns ?",
+    optionA: "1000",
+    optionB: "1024",
+    optionC: "1028",
+    optionD: "1030",
+    correctOption: "optionC",
+  },
+
+  {
+    question: "Which of the following age is also known as copper age ?",
+    optionA: "Paleolithic age",
+    optionB: " Neolithic age",
+    optionC: "Mesolithic age",
+    optionD: "Chalcolithic age",
+    correctOption: "optionD",
+  },
+
+  {
+    question:
+      "Which among the following major rock edicts of Ashoka are in Kharosthi script ?",
+    optionA: " Mansehra and Sahbazgarhi",
+    optionB: "Sanchi",
+    optionC: "Sarnath",
+    optionD: "Bairut",
+    correctOption: "optionA",
+  },
+
+  {
+    question:
+      "Which among the following veda has been described as “Braham Veda ?",
+    optionA: "Rigveda",
+    optionB: "Samaveda",
+    optionC: "Yajurveda",
+    optionD: "Atharavaveda",
+    correctOption: "optionD",
+  },
+
+  {
+    question:
+      "Who was the earliest known Chola King who conquered Sri Lanka and ruled it  ?",
+    optionA: "Karikala",
+    optionB: "Elara",
+    optionC: "Udiyangera",
+    optionD: "Nedunjelian",
+    correctOption: "optionB",
+  },
+
+  {
+    question:
+      "Which of the following temples from the Gupta per?Which of the following temples from the Gupta period is entirely made up of Bricks?",
+    optionA: "Dasavatar Temple",
+    optionB: " Bhitargaon temple",
+    optionC: "Shiva Temple of Bhumara",
+    optionD: "Shiva Temple of Koh",
+    correctOption: "optionB",
+  },
+
+  {
+    question:
+      "Jejakabhukti was the ancient name of which of the following places ?",
+    optionA: "Ujjain",
+    optionB: "Mathura",
+    optionC: "Bundelkhand",
+    optionD: "Rajasthan",
+    correctOption: "optionC",
+  },
+
+  {
+    question:
+      "Who was the ruler of the kingdom between the rivers Jhelum and Chenab?",
+    optionA: " Alexander the Great",
+    optionB: "Darius III",
+    optionC: "King Porus",
+    optionD: " Chandragupta Maurya",
+    correctOption: "optionC",
+  },
+];
+
+let shuffledQuestions = []; //empty array to hold shuffled selected questions out of all available questions
+
+function handleQuestions() {
+  //function to shuffle and push 10 questions to shuffledQuestions array
+  //app would be dealing with 10questions per session
+  while (shuffledQuestions.length <= 9) {
+    const random = questions[Math.floor(Math.random() * questions.length)];
+    if (!shuffledQuestions.includes(random)) {
+      shuffledQuestions.push(random);
+    }
+  }
+}
+
+let questionNumber = 1; //holds the current question number
+let playerScore = 0; //holds the player score
+let wrongAttempt = 0; //amount of wrong answers picked by player
+let indexNumber = 0; //will be used in displaying next question
+
+// function for displaying next question in the array to dom
+//also handles displaying players and quiz information to dom
+function NextQuestion(index) {
+  handleQuestions();
+  const currentQuestion = shuffledQuestions[index];
+  document.getElementById("question-number").innerHTML = questionNumber;
+  document.getElementById("player-score").innerHTML = playerScore;
+  document.getElementById("display-question").innerHTML =
+    currentQuestion.question;
+  document.getElementById("option-one-label").innerHTML =
+    currentQuestion.optionA;
+  document.getElementById("option-two-label").innerHTML =
+    currentQuestion.optionB;
+  document.getElementById("option-three-label").innerHTML =
+    currentQuestion.optionC;
+  document.getElementById("option-four-label").innerHTML =
+    currentQuestion.optionD;
+}
+
+function checkForAnswer() {
+  const currentQuestion = shuffledQuestions[indexNumber]; //gets current Question
+  const currentQuestionAnswer = currentQuestion.correctOption; //gets current Question's answer
+  const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+  let correctOption = null;
+
+  options.forEach((option) => {
+    if (option.value === currentQuestionAnswer) {
+      //get's correct's radio input with correct answer
+      correctOption = option.labels[0].id;
+    }
+  });
+
+  //checking to make sure a radio input has been checked or an option being chosen
+  if (
+    options[0].checked === false &&
+    options[1].checked === false &&
+    options[2].checked === false &&
+    options[3].checked == false
+  ) {
+    document.getElementById("option-modal").style.display = "flex";
+  }
+
+  //checking if checked radio button is same as answer
+  options.forEach((option) => {
+    if (option.checked === true && option.value === currentQuestionAnswer) {
+      document.getElementById(correctOption).style.backgroundColor = "green";
+      playerScore++; //adding to player's score
+      indexNumber++; //adding 1 to index so has to display next question..
+      //set to delay question number till when next question loads
+      setTimeout(() => {
+        questionNumber++;
+      }, 1000);
+    } else if (option.checked && option.value !== currentQuestionAnswer) {
+      const wrongLabelId = option.labels[0].id;
+      document.getElementById(wrongLabelId).style.backgroundColor = "red";
+      document.getElementById(correctOption).style.backgroundColor = "green";
+      wrongAttempt++; //adds 1 to wrong attempts
+      indexNumber++;
+      //set to delay question number till when next question loads
+      setTimeout(() => {
+        questionNumber++;
+      }, 1000);
+    }
+  });
+}
+
+//called when the next button is called
+function handleNextQuestion() {
+  checkForAnswer(); //check if player picked right or wrong option
+  unCheckRadioButtons();
+  //delays next question displaying for a second just for some effects so questions don't rush in on player
+  setTimeout(() => {
+    if (indexNumber <= 9) {
+      //displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+      NextQuestion(indexNumber);
+    } else {
+      handleEndGame(); //ends game if index number greater than 9 meaning we're already at the 10th question
+    }
+    resetOptionBackground();
+  }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+  const options = document.getElementsByName("option");
+  options.forEach((option) => {
+    document.getElementById(option.labels[0].id).style.backgroundColor = "";
+  });
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+  const options = document.getElementsByName("option");
+  for (let i = 0; i < options.length; i++) {
+    options[i].checked = false;
+  }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+  let remark = null;
+  let remarkColor = null;
+
+  // condition check for player remark and remark color
+  if (playerScore <= 3) {
+    remark = "Bad Grades, Keep Practicing BABYBOY.";
+    remarkColor = "red";
+  } else if (playerScore >= 4 && playerScore < 7) {
+    remark = "Average Grades, You can do better BABYBOY.";
+    remarkColor = "orange";
+  } else if (playerScore >= 7) {
+    remark = "Excellent, Keep the good work going BABYBOY.";
+    remarkColor = "green";
+  }
+  const playerGrade = (playerScore / 10) * 100;
+
+  //data to display to score board
+  document.getElementById("remarks").innerHTML = remark;
+  document.getElementById("remarks").style.color = remarkColor;
+  document.getElementById("grade-percentage").innerHTML = playerGrade;
+  document.getElementById("wrong-answers").innerHTML = wrongAttempt;
+  document.getElementById("right-answers").innerHTML = playerScore;
+  document.getElementById("score-modal").style.display = "flex";
+}
+
+//closes score modal, resets game and reshuffles questions
+function closeScoreModal() {
+  questionNumber = 1;
+  playerScore = 0;
+  wrongAttempt = 0;
+  indexNumber = 0;
+  shuffledQuestions = [];
+  NextQuestion(indexNumber);
+  document.getElementById("score-modal").style.display = "none";
+}
+
+//function to close warning modal
+function closeOptionModal() {
+  document.getElementById("option-modal").style.display = "none";
+}
